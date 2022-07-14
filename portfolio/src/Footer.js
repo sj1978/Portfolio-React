5835707bc07cde3c66acfc8e6d9b5fc77c2f73b7
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-07-13 23:37:42 -0300

    add text to footer

--- portfolio/src/Footer.js
+++ portfolio/src/Footer.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import './Footer.css'
 
-export const Footer = () => {
+const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer">
+    <p className='copy'>Copyright &copy; 2022</p>
+    </div>
   )
 }
+
+export default Footer
