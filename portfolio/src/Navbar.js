a2a422afa1c5e98bbf0c58a9bf1deacff5c633d5
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-07-21 02:37:21 -0300

    add icons and functionality

--- portfolio/src/Navbar.js
+++ portfolio/src/Navbar.js
@@ -1,13 +1,34 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import { useState } from 'react'
+import * as AiIcons from 'react-icons/ai'
+import * as FaIcons from 'react-icons/fa'
 
 
 
 const Navbar = () => {
+    const [sidebar, setSidebar] = useState(false);
+
+    const showSidebar = () => {
+        setSidebar(!sidebar);
+    }
   return (
+    <>
     <div className="navbar">
-<ul>
-<li>
+<Link to='#' className='menu-bars' >
+        <FaIcons.FaBars  onClick={showSidebar} />
+      </Link>                 
+    </div>
+
+
+     <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+      <ul className='nav-menu-items'>
+        <li className='navbar-toggle'>
+          <Link to='#' className='menu-bars'>
+            <AiIcons.AiOutlineClose/>
+          </Link>
+        </li>
+<li className='navbar-toggle'>
   <Link to="/"> Home</Link>
   </li>
   <li> 
@@ -21,8 +42,8 @@ const Navbar = () => {
    </li>
 </ul>
 
-    </div>
-  
+</nav>
+   </>
   )
 }
 
